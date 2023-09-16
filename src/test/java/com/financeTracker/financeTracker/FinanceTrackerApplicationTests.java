86e6220baa65d38f1c324d9486f92252f23ea864
tligali <tligali@paga.com>
2023-09-16 22:53:12 +0100

    First commit

---
+++ src/test/java/com/financeTracker/financeTracker/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeTracker.financeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
